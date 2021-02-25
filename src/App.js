bb5dcb6db0617e8ba8bc0f45f18c87fbe30bb4db
juancaacevedo10 <63076635+juancaacevedo10@users.noreply.github.com>
2021-02-25 01:53:14 -0500

    the project was created and the interface template was designed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h3 className='text-center'>VETERINARIA</h3>
+      <hr/>
+      <div className='text-center'>
+      <button className='btn btn-primary btn-lg rounded-pill'>Nuevo paciente <i class="fas fa-plus-circle"></i></button>
+      </div>
+      <hr/>
+      <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">Nombre mascota</th>
+      <th scope="col">Tipo mascota</th>
+      <th scope="col">Raza</th>
+      <th scope="col">Fecha de nacimiento de la mascota </th>
+      <th scope="col">Nombre y apellido propietario</th>
+      <th scope="col">Telefono propietario </th>
+      <th scope="col">Direccion Propietario </th>
+      <th scope="col">Email propietario </th>
+      <th scope="col">Acciones </th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>michi</th>
+      <td>gato</td>
+      <td>Otto</td>
+      <td>10-08-1997</td>
+      <td>juan acevedo</td>
+      <td>321321234</td>
+      <td>cll 88 n 45 a 87</td>
+      <td>jua@gmail.com</td>
+      <td><button className='btn btn-warning rounded-circle '><i className="far fa-edit"></i></button><button className='btn btn-danger rounded-circle '><i className="fas fa-trash-alt"></i></button> </td>
+    </tr>
+    <tr>
+      <th>michi</th>
+      <td>gato</td>
+      <td>Otto</td>
+      <td>10-08-1997</td>
+      <td>juan acevedo</td>
+      <td>321321234</td>
+      <td>cll 88 n 45 a 87</td>
+      <td>jua@gmail.com</td>
+      <td><button className='btn btn-warning rounded-circle '><i className="far fa-edit"></i></button><button className='btn btn-danger rounded-circle '><i className="fas fa-trash-alt"></i></button> </td>
+    </tr>
+  </tbody>
+</table>
     </div>
   );
 }
